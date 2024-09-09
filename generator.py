0094d5ccd536c290adbf269eba6a8c4c5c92a077
Gabriel Sagan <sagangabe@gmail.com>
2024-09-09 14:05:46 -0400

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,40 @@
+from scamp import *
+from melody import *
+import time
+import random
+
+trichord_input()
+duration_input()
+temp = tempo_input()
+
+while True:
+    
+    s = None
+    
+    if isinstance(temp, list):
+        if len(temp) == 2: 
+            temp_int = [int(num) for num in temp]
+            s = Session (tempo = random.randint(min(temp_int), max(temp_int)))
+    else:
+        s = Session(tempo = int(temp))
+    
+    #Use instead if wanting to use own plugin
+    #p = s.new_midi_part("piano", midi_output_device="loopMIDI Port")
+    p = s.new_part("piano")
+    
+    #s.start_transcribing()
+
+    melody = make_melody()
+    
+    print(s)
+    print(melody)
+    
+    if melody:
+        for note, duration in melody:
+            p.play_note(note, 0.7, duration)
+    
+    #performance = s.stop_transcribing()
+    #performance.to_score(title="RTMP 0.4", composer = "Generated with Real Time Midi Program 0.4 written by Gabriel Sagan").show_xml()
+
+
+    wait(random.randint(2, 7))
---
+++ melody.py
@@ -0,0 +1,218 @@
+import random
+
+forte_numbers = {
+    '3-1': [36, 37, 38],
+    '3-2': [36, 37, 39],
+    '3-2i': [36, 38, 39],
+    '3-3': [36, 37, 40],
+    '3-3i': [36, 39, 40],
+    '3-4': [36, 37, 41],
+    '3-4i': [36, 40, 41],
+    '3-5': [36, 37, 42],
+    '3-5i': [36, 41, 42],
+    '3-6': [36, 38, 40],
+    '3-7': [36, 38, 41],
+    '3-7i': [36, 39, 41],
+    '3-8': [36, 38, 42],
+    '3-8i': [36, 40, 42],
+    '3-9': [36, 38, 43],
+    '3-10': [36, 39, 42],
+    '3-11': [36, 39, 43],
+    '3-11i': [36, 40, 43],
+    '3-12': [36, 40, 44]
+}
+
+note_values = {
+    
+    '16th notes': .25,
+    '8th notes': .5,
+    'quarter notes': 1,
+    'half notes': 2,
+    'whole notes': 4
+}
+
+trichords = []
+durations = []
+
+def trichord_input():
+    
+    global trichords
+    
+    while True: 
+        selected_chords_str = input("Enter trichord set classes separated by comma. Use i for inverted trichords. (e.g. 3-9, 3-5i, 3-8) or type 'All' for all chords: ")
+        
+        if selected_chords_str.strip().lower() == 'all':
+            trichords = list(forte_numbers.values())
+            print(f"Selected All Chords")
+            return trichords
+        
+        selected_chords_list = selected_chords_str.split(',')
+        selected_chords_list = [chord.strip() for chord in selected_chords_list]
+        
+        trichords = list(forte_numbers[numbers] for numbers in selected_chords_list if numbers in forte_numbers)
+        
+        try:
+            if all(chord in forte_numbers for chord in selected_chords_list):
+                print(f"Selected Chords: {selected_chords_list}")
+                return trichords
+            else:
+                raise ValueError("Invalid set class input")
+
+        except ValueError as e:
+            print(f"{e}, please try again.")
+
+def duration_input():
+    
+    global durations
+    
+    while True:
+        selected_duration_str = input("Enter note values separated by comma. (e.g. 16th notes, 8th notes, quarter notes, half notes, whole notes) or type 'All' for all note values: ")
+        
+        if selected_duration_str.strip().lower() == 'all':
+            durations = list(note_values.values())
+            print(f"Selected All Note Values")
+            return durations
+        
+        selected_duration_list = selected_duration_str.split(',')
+        selected_duration_list = [num.strip() for num in selected_duration_list]
+        
+        durations = list(note_values[num] for num in selected_duration_list if num in note_values)
+        
+        try:
+            if all(num in note_values for num in selected_duration_list):
+                print(f"Selected Note Values: {durations}")
+                return durations
+            else:
+                raise ValueError("Invalid note value input")
+            
+        except ValueError as e:
+            print(f"{e}, please try again.")
+            
+def tempo_input():
+
+    while True:
+        
+        choice = input("Should there be a random tempo selected? (Y/N): ").lower()
+        if choice == 'y' or choice == 'yes':
+            while True:
+                try:
+                    tempo_range = input("What should the tempo range be? Please separate by comma (e.g. 60, 75): ")
+                    tempo_range = tempo_range.split(',')
+                    tempo_range_int = [int(num.strip()) for num in tempo_range]
+                    
+                    if len(tempo_range_int) == 2:
+                        if tempo_range_int[0] < tempo_range_int[1]:                          
+                            print(f"Tempo range: {tempo_range_int}")
+                            return tempo_range_int
+                        else:
+                            raise ValueError("The first number must be less than the second number.")
+                    else:
+                        raise ValueError("Please enter exactly two numbers separated by a comma.")
+                        
+                except ValueError as e:
+                    print(f"{e} Please try again.")
+        
+        elif choice == 'n' or choice == 'no':
+            
+            while True:  
+                tempo = input("Enter the tempo to be used: ")
+                
+                try:   
+                    if int(tempo):
+                        print(f'Selected BPM = {tempo}')
+                        return tempo
+                        
+                    else:
+                        raise ValueError("Invalid input")
+                        
+                except ValueError as e:
+                    print(f"{e}, please try again.")
+        else:
+            print('Please input Y/N')
+    
+
+def note_input():
+    
+    while True:
+        
+        choice = input("Should there be random number of notes selected? (Y/N): ").lower()
+        if choice == 'y' or choice == 'yes':
+            while True:
+                try:
+                    note_range = input("What should the note number range be? Please separate by comma (e.g. 6, 36): ")
+                    note_range = note_range.split(',')
+                    note_range_int = [int(num.strip()) for num in note_range]
+                    
+                    if len(note_range_int) == 2:
+                        if note_range_int[0] < note_range_int[1]:                          
+                            print(f"Note number range: {note_range_int}")
+                            return note_range_int
+                        else:
+                            raise ValueError("The first number must be less than the second number.")
+                    else:
+                        raise ValueError("Please enter exactly two numbers separated by a comma.")
+                        
+                except ValueError as e:
+                    print(f"{e} Please try again.")
+        
+        elif choice == 'n' or choice == 'no':
+            
+            while True:  
+                note = input("Enter the number of notes: ")
+                
+                try:   
+                    if int(note):
+                        print(f'Selected number = {note}')
+                        return note
+                        
+                    else:
+                        raise ValueError("Invalid input")
+                        
+                except ValueError as e:
+                    print(f"{e}, please try again.")
+        else:
+            print('Please input Y/N')
+            
+
+notes = note_input()
+
+def three():
+    
+    if isinstance(notes, list):
+        if len(notes) == 2:
+            notes_int = [int(num) for num in notes]
+            random_int = random.randint(min(notes_int), max(notes_int))
+            return random_int
+                
+    else:
+        return int(notes)
+            
+
+def make_melody():
+    
+    melody = []
+     
+    num_notes = three()
+            
+    trichord_seq = random.choices(trichords, k=num_notes)
+    
+    for count in range(len(trichord_seq)):
+        random_trichord = trichord_seq[count]
+        random_transposition = random.randint(0, 36)
+        transposed_trichords = [num + random_transposition for num in random_trichord]
+        trichord_seq[count] = transposed_trichords
+    
+    random.shuffle(trichord_seq)
+    
+    for trichord in trichord_seq:
+        random.shuffle(trichord)
+        
+    while len(melody) < num_notes:
+        for trichord in trichord_seq:
+            for note in trichord:
+                if len(melody) < num_notes:
+                    duration = random.choice(durations)
+                    melody.append([note, duration])
+        
+    return melody
+    
