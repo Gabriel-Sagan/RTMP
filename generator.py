95944eda73dabfefbac70305be14ed4c4c385315
Gabriel Sagan <sagangabe@gmail.com>
2024-09-16 22:41:15 -0400

    Upload corrections

--- generator.py
+++ generator.py
@@ -18,28 +18,28 @@ while True:
     else:
         s = Session(tempo = int(temp))
     
-    #Use instead if wanting to use own plugin
-    #p = s.new_part("piano")
-    p = s.new_midi_part("piano", midi_output_device="loopMIDI Port")
+    #Use instead if wanting to use external plugin
+    #Windows: p = s.new_midi_part("piano", midi_output_device="loopMIDI Port")
+    #Mac: p = s.new_midi_part("piano", midi_output_device="IAC")
+    #Linux: p = s.new_midi_part("piano", midi_output_device="Midi Through Port 0")
     
-    
-    #s.start_transcribing()
+    p = s.new_part("piano")
     
     melody = make_melody()
-   
+    
+    #s.start_transcribing()
+
     pitch_classes = [(note % 12, duration) for note, duration in melody]   
     
     print(s)
     print(pitch_classes)
     
-    
     if melody:
         for note, duration in melody:
             p.play_note(note, 0.7, duration)
-            
     
     #performance = s.stop_transcribing()
-    #performance.to_score(title="RTMP 0.4", composer = "Generated with Real Time Midi Program 0.4 written by Gabriel Sagan").show_xml()
+    #performance.to_score(title="RTMP", composer = "Generated with Real Time Midi Program written by Gabriel Sagan").show_xml()
 
 
     wait(random.randint(2, 7))
